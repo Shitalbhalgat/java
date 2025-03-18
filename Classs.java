65f7f450b7c34fece70d3f19c63639f8747ad9ae
Shitalbhalgat <shitalbhalgat@gmail.com>
2025-03-18 10:39:34 +0530

    method

---
+++ Classs.java
@@ -0,0 +1,22 @@
+class Demo
+{
+    int x;
+    void f1()
+    {
+        System.out.println("From Demo class");
+    }
+    
+}
+public class Classs {
+    public static void main(String args[])
+    {
+       Demo d=new Demo();
+       d.f1();
+       d.x=10;
+       System.out.println(d.x);
+
+
+    }
+
+    
+}
