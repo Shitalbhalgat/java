b0ba4d5c9f41b462dbc5f07ed0371be3de7dba3f
Shitalbhalgat <shitalbhalgat@gmail.com>
2025-03-26 12:00:49 +0530

    26-3

---
+++ Static_variable.java
@@ -0,0 +1,29 @@
+public class Static_variable {
+    //Static Variable
+    int count ;
+    Static_variable()
+    {
+        count++;
+        System.out.println(count);
+    }
+
+   //Staic block
+    static
+    {
+        //  count= 10;
+        System.out.println("Staic block"+a);
+    }
+
+    public static void main(String[] args) {
+        Static_variable s1= new Static_variable();
+        Static_variable s2= new Static_variable();
+        Static_variable s3= new Static_variable();
+        s3.count=3;
+        System.out.println(s3.count);
+        Static_variable s4=s3;
+        System.out.println(s4.count);
+
+        
+    }
+//     
+}
