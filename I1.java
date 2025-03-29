0db83b41b2cbd292729ea18e9f0930cc4184d3d5
Shitalbhalgat <shitalbhalgat@gmail.com>
2025-03-29 11:54:53 +0530

    interface

---
+++ I1.java
@@ -0,0 +1,21 @@
+public interface I1 {
+   
+    void f1();
+  default  void display()
+    {
+
+    }
+    static void r()
+    {
+    }
+    private void f2(){}
+}
+class A implements I1{
+
+}
+public class Interfaces {
+    public static void main(String[] args) {
+        
+    }
+    
+}
