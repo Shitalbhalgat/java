93e22a9f05e4bfd457d140d581354d255e82ad95
Shitalbhalgat <shitalbhalgat@gmail.com>
2026-02-08 21:37:12 +0530

    package

---
+++ Staticimport.java
@@ -0,0 +1,11 @@
+// Static import allows you to use static members (variables or methods) of a class
+// without writing the class name again and again.
+
+import  java.lang.Math.*;
+class Staticimport {
+    public static void main(String[] args) {
+
+        double x = Math.sqrt(25);
+        System.out.println(Math.PI);
+    }
+}
