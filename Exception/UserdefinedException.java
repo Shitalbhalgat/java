b2c68ecb37b366bb6207ab637b5a2cfba2c3841e
Shitalbhalgat <shitalbhalgat@gmail.com>
2026-02-15 22:59:28 +0530

    Exception

--- Exception/UserdefinedException.java
+++ Exception/UserdefinedException.java
@@ -1,3 +1,17 @@
+// User defined exception : 1) A User Defined Exception is an exception created by the programmer to handle 
+//                          application-specific errors that are not covered by built-in exceptions.
+//                          2)It is created by extending the Exception or RuntimeException class.
+// Steps to Create User Defined Exception
+// 1) Define a new class that extends the Exception class(checked exceptions) or RuntimeException (for unchecked exceptions).
+    //   syntax: class MyException extends Exception
+	//                 { 
+	//                 ... }
+// 2) Implement a constructor that accepts a message and passes it to the superclass constructor.
+// 3) Optionally, override the toString() method to provide a custom string representation of the exception.
+// 4) Throw the exception in your code when a specific error condition occurs using the throw keyword.
+// 5) Catch the exception using a try-catch block to handle it.
+
+
 import java.util.Scanner;
 
 class AgenotmatchException extends Exception 
