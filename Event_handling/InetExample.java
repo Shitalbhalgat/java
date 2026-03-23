787db401581f7806a80695d94c9782fd604aeada
Shitalbhalgat <shitalbhalgat@gmail.com>
2026-03-24 00:07:28 +0530

    Networking
    Updated comments for clarity and added output for local IP.

--- Event_handling/InetExample.java
+++ Event_handling/InetExample.java
@@ -1,4 +1,10 @@
-//  InetAddress class represents IP address of a host. It provides methods to get host name, IP address, and check connectivity using methods like getByName(), getHostAddress(), and isReachable().
+// The InetAddress class (from java.net package) is used to represent an IP address.
+// It helps to:  Get IP address of a host
+//               Get host name
+//               Work with DNS (Domain Name System)
+// It provides methods to get host name, IP address, 
+// and check connectivity using methods like getByName(), getHostAddress(), and isReachable().
+// Note:Each Method of InetAddress class may throw an UnknownHostException if the host is not found or cannot be resolved.
 import java.net.*;
 
 public class InetExample {
@@ -6,16 +12,16 @@ public class InetExample {
         try {
             // Returns IP address of a host
             InetAddress ip = InetAddress.getByName("www.google.com");
+            System.out.println(ip);
 
             System.out.println("Host Name: " + ip.getHostName());
             System.out.println("IP Address: " + ip.getHostAddress());
-            System.out.println("Canonical Name: " + ip.getCanonicalHostName());
-
+           
             System.out.println("Reachable: " + ip.isReachable(5000));
 
             // Returns your system IP
             InetAddress ip1 = InetAddress.getLocalHost();
-            System.out.println(ip1);
+            System.out.println("IP Address of your system: " + ip1);
 
         } catch(Exception e) {
             System.out.println(e);
