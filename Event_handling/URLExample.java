e31cd1f37272a173b6b13dd0be24689a493ce07f
Shitalbhalgat <shitalbhalgat@gmail.com>
2026-03-24 00:07:59 +0530

    ne
    Added query and reference output to URLExample.

--- Event_handling/URLExample.java
+++ Event_handling/URLExample.java
@@ -3,8 +3,8 @@
 // 
 // https://www.google.com:443/index.html?name=abc#top
 // │      │               │   │             │        │
-// │      │               │   │             │        └── Ref
-// │      │               │   │             └──────── Query
+// │      │               │   │             │        └──Reference (Section in page)
+// │      │               │   │             └──────── Query(Data sent to server)
 // │      │               │   └───────────── Path
 // │      │               └───────────────── Port
 // │      └──────────────────────────────── Host
@@ -19,10 +19,12 @@ public class URLExample {
 
             System.out.println("Protocol: " + u.getProtocol());
             System.out.println("Host: " + u.getHost());
-            System.out.println("Port: " + u.getPort());
-            System.out.println("Default Port: " + u.getDefaultPort());
+            System.out.println("Port: " + u.getPort()); //Returns -1 if the port is not specified in the URL
+            System.out.println("Default Port: " + u.getDefaultPort());  //Returns the default port for the protocol
             System.out.println("File: " + u.getFile());
             System.out.println("Path: " + u.getPath());
+            System.out.println("Query: " + u.getQuery());
+            System.out.println("Reference: " + u.getRef());
 
         } catch(Exception e) {
             System.out.println(e);
